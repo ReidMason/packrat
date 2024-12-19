e58764ae955bc7d4588cc376299bbc78f8b78e56
Reid Mason <maddogshain132@gmail.com>
2024-12-19 00:58:59 +0000

    refactor: corrected warnings by making dto request fields required

--- packrat-api/packrat.webapi/Endpoints/Registration/Dtos/RegisterRequestDto.cs
+++ packrat-api/packrat.webapi/Endpoints/Registration/Dtos/RegisterRequestDto.cs
@@ -2,6 +2,6 @@ namespace packrat.webapi.Endpoints.Registration.Dtos;
 
 public class RegisterRequestDto
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
