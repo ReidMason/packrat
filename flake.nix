b7623af66e85f046eecc861428e6c4995f9e97a0
Ddraigan <lkjjones1999@gmail.com>
2026-04-24 21:51:39 +0100

    feat: nix support

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "An inventory manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+
+    eachSystem = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f system nixpkgs.legacyPackages.${system});
+  in {
+    packages = eachSystem (system: pkgs: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        name = "packrat";
+        src = ./.;
+        buildInputs = [pkgs.glib];
+        nativeBuildInputs = [pkgs.pkg-config];
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    });
+
+    devShells = eachSystem (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+  };
+}
