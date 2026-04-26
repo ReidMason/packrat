dc94b4296af7957ace5f890e2cdf55535e63211b
Ddraigan <lkjjones1999@gmail.com>
2026-04-26 14:11:27 +0100

    fix: dioxus targets

--- flake.nix
+++ flake.nix
@@ -18,9 +18,21 @@
       overlays = [(import rust-overlay)];
       pkgs = import nixpkgs {
         inherit system overlays;
+        config = {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        };
       };
 
-      # Specific dependencies for Dioxus Desktop/Fullstack
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        includeNDK = true;
+        ndkVersion = "26.1.10909125";
+        platformVersions = ["34"];
+        buildToolsVersions = ["34.0.0"];
+        abiVersions = ["x86_64" "arm64-v8a"];
+        includeExtras = ["extras;google;m2repository"];
+      };
+
       runtimeDeps = with pkgs; [
         webkitgtk_4_1
         gtk3
@@ -31,6 +43,10 @@
         openssl_3
         librsvg
         xdotool
+        # GStreamer
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
       ];
 
       buildInputs = with pkgs;
@@ -38,7 +54,11 @@
           # Rust toolchain with WASM support for Web
           (rust-bin.stable.latest.default.override {
             extensions = ["rust-src" "rust-analyzer"];
-            targets = ["wasm32-unknown-unknown"];
+            targets = [
+              "wasm32-unknown-unknown"
+              "aarch64-linux-android"
+              "x86_64-linux-android"
+            ];
           })
 
           # Dioxus CLI and tools
@@ -47,6 +67,10 @@
           trunk
           cargo-tarpaulin
           sqlx-cli
+
+          # Android
+          android-tools
+          androidComposition.androidsdk
         ]
         ++ runtimeDeps;
     in {
@@ -56,6 +80,15 @@
         # Ensures Cargo can find libraries for Desktop compilation
         shellHook = ''
           export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath runtimeDeps}"
+          export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
+
+          export ANDROID_HOME="${androidComposition.androidsdk}/libexec/android-sdk"
+          export ANDROID_NDK_HOME="$ANDROID_HOME/ndk-bundle"
+          export JAVA_HOME="${pkgs.jdk17.home}"
+          export GRADLE_USER_HOME="$PWD/.gradle"
+          export ANDROID_SDK_ROOT="$ANDROID_HOME"
+          export GRADLE_OPTS="-Dandroid.aapt2FromMaven=false"
+
           dx --version
         '';
       };
