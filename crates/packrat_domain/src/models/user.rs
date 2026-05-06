883f79f1cf96be1ecfe46f06e74673f7e910b71c
Ddraigan <lkjjones1999@gmail.com>
2026-05-06 22:21:05 +0100

    feat: user model - password and username

--- crates/packrat_domain/src/models/user.rs
+++ crates/packrat_domain/src/models/user.rs
@@ -1,4 +1,55 @@
+use argon2::{PasswordHasher, PasswordVerifier};
+
 use super::entity::EntityTimestamp;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct PasswordHash(String);
+
+impl PasswordHash {
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+
+    pub fn verify(&self, plain_password: &str) -> bool {
+        if let Ok(parsed_hash) = argon2::PasswordHash::new(&self.0) {
+            return argon2::Argon2::default()
+                .verify_password(plain_password.as_bytes(), &parsed_hash)
+                .is_ok();
+        }
+        false
+    }
+
+    pub fn generate(plain_password: &str) -> Result<Self, String> {
+        let salt = argon2::password_hash::SaltString::generate(
+            &mut argon2::password_hash::rand_core::OsRng,
+        );
+
+        let hashed_string = argon2::Argon2::default()
+            .hash_password(plain_password.as_bytes(), &salt)
+            .map_err(|err| format!("Password hashing failed: {}", err))?
+            .to_string();
+
+        Ok(Self(hashed_string))
+    }
+}
+
+impl From<String> for PasswordHash {
+    fn from(value: String) -> Self {
+        PasswordHash(value)
+    }
+}
+
+impl From<PasswordHash> for String {
+    fn from(value: PasswordHash) -> Self {
+        value.0
+    }
+}
+
+impl From<&str> for PasswordHash {
+    fn from(value: &str) -> Self {
+        PasswordHash(value.to_string())
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct UserId(i64);
@@ -43,9 +94,38 @@ impl From<Email> for String {
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Username(String);
+
+impl Username {
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl From<&str> for Username {
+    fn from(s: &str) -> Self {
+        Username(s.to_string())
+    }
+}
+
+impl From<String> for Username {
+    fn from(s: String) -> Self {
+        Username(s)
+    }
+}
+
+impl From<Username> for String {
+    fn from(username: Username) -> Self {
+        username.0
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct User {
     pub id: UserId,
     pub email: Email,
+    pub username: Username,
+    pub password: PasswordHash,
     pub created: EntityTimestamp,
     pub updated: EntityTimestamp,
 }
@@ -54,12 +134,16 @@ impl User {
     pub fn new(
         id: UserId,
         email: Email,
+        username: Username,
+        password: PasswordHash,
         created: EntityTimestamp,
         updated: EntityTimestamp,
     ) -> Self {
         Self {
             id,
             email,
+            username,
+            password,
             created,
             updated,
         }
